ab5734e7b65298eaba77469c20d472846f6fdb78
DebaJoey <109353693+DebaJoey@users.noreply.github.com>
2023-03-07 16:43:21 +0000

    Add files via upload

---
+++ ECommerce.js
@@ -0,0 +1,142 @@
+const wrapper = document.querySelector('.sliderWrapper')
+const menuItems = document.querySelectorAll('.menuItem')
+
+
+
+const products = [
+    {
+      id: 1,
+      title: "Air Force",
+      price: '$'+119,
+      colors: [
+        {
+          code: "black",
+          img: "./img/air.png",
+        },
+        {
+          code: "darkblue",
+          img: "./img/air2.png",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Air Jordan",
+      price: '$'+149,
+      colors: [
+        {
+          code: "lightgray",
+          img: "./img/jordan.png",
+        },
+        {
+          code: "green",
+          img: "./img/jordan2.png",
+        },
+      ],
+    },
+    {
+      id: 3,
+      title: "Blazer",
+      price: '$'+109,
+      colors: [
+        {
+          code: "lightgray",
+          img: "./img/blazer.png",
+        },
+        {
+          code: "green",
+          img: "./img/blazer2.png",
+        },
+      ],
+    },
+    {
+      id: 4,
+      title: "Crater",
+      price: '$'+129,
+      colors: [
+        {
+          code: "black",
+          img: "./img/crater.png",
+        },
+        {
+          code: "lightgray",
+          img: "./img/crater2.png",
+        },
+      ],
+    },
+    {
+      id: 5,
+      title: "Hippie",
+      price: '$'+99,
+      colors: [
+        {
+          code: "gray",
+          img: "./img/hippie.png",
+        },
+        {
+          code: "black",
+          img: "./img/hippie2.png",
+        },
+      ],
+    },
+  ];
+
+  let chosenProduct = products[0];
+
+  const currentProductImg = document.querySelector('.productImg')
+  const currentProductTitle = document.querySelector('.productTitle')
+  const currentProductPrice = document.querySelector('.productPrice')
+  const currentProductColors = document.querySelectorAll('.color')
+  const currentProductSizes = document.querySelectorAll('.size')
+
+
+menuItems.forEach((item,index)=> {
+    item.addEventListener('click', () => {
+        wrapper.style.transform =`translateX(${-100 * index}vw)`;
+
+        chosenProduct = products[index]
+        currentProductTitle.textContent = chosenProduct.title
+        currentProductPrice.textContent = chosenProduct.price
+        currentProductImg.src = chosenProduct.colors[0].img
+
+        currentProductColors.forEach((col,ind)=> {
+           
+                col.style.backgroundColor = chosenProduct.colors[ind].code
+            
+            })
+    })
+} )
+
+
+currentProductColors.forEach((col,ind) => {
+    col.addEventListener('click', () => {
+        currentProductImg.src = chosenProduct.colors[ind].img
+    })
+})
+
+currentProductSizes.forEach((col,ind) =>{
+    col.addEventListener('click', ()=>{
+        currentProductSizes.forEach((col) =>{
+          col.style.backgroundColor = 'white'
+            col.style.color = 'black'
+        })
+        col.style.backgroundColor = 'black'
+        col.style.color = 'white'
+      
+    })
+})
+
+const productButton = document.querySelector(".productButton")
+const payment = document.querySelector(".payment")
+const closed = document.querySelector(".close")
+
+productButton.addEventListener('click', () => {
+     payment.style.display = 'flex'
+    
+});
+
+closed.addEventListener('click', () => {
+    payment.style.display = 'none';
+    
+})
+
